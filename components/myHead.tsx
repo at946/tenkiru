8fc3703447bfcadf3344f507a7a19eb6f8548495
Asato TAKAHASHI <asato730@gmail.com>
2022-10-08 20:17:42 +0900

    add metatags

--- components/myHead.tsx
+++ components/myHead.tsx
@@ -2,14 +2,21 @@ import { NextPage } from "next";
 import Head from "next/head";
 
 const MyHead: NextPage = () => {
-  const title = 'テンキル'
-  const description = 'オンラインプランニングポーカーツール'
+  const title = 'Tenkir'
+  const description = 'Tenkir（テンキル）はオンラインプランニングポーカーツールです。チームメンバーでわいわい使ってね。'
 
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/ogp.jpg`} />
+      <meta property="twitter:card" content="summary" />
     </Head>
   )
 }

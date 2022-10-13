a5cbff3246419eb22dd807c1090aa45836ffc856
Asato TAKAHASHI <asato730@gmail.com>
2022-10-13 19:32:36 +0900

    add e2e

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,32 @@
-import type { NextPage } from 'next';
+import type { NextPage, GetServerSideProps } from 'next';
 import Link from 'next/link';
 import { v4 } from 'uuid';
 
-const Home: NextPage = () => {
+type Props = {
+  roomId: string;
+}
+
+const Home: NextPage<Props> = ({ roomId }: Props) => {
   return (
     <div className='has-text-centered'>
       <section className='section'>
         <h1 className='title has-text-primary'>Tenkir</h1>
         <h2 className='subtitle has-text-dark'>Online Planning Poker Tool</h2>
-        <Link href={`/rooms/${encodeURIComponent(v4())}`}>
-          <a className='button is-rounded is-outlined is-primary mt-5'>Create a room</a>
+        <Link href={`/rooms/${encodeURIComponent(roomId)}`}>
+          <a className='button is-rounded is-outlined is-primary mt-5' data-testid="createRoomButton">Create a room</a>
         </Link>
       </section>
     </div>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const props: Props = {
+    roomId: v4()
+  }
+  return {
+    props: props
+  }
+}
 
 export default Home;

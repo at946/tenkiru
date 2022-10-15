c1394cbc7ec43c8ad0265df75bd81a7d9171bd62
Lint Action <lint-action@samuelmeuli.com>
2022-10-15 15:09:18 +0000

    Fix code style issues with Prettier

--- components/deckSelect.tsx
+++ components/deckSelect.tsx
@@ -15,12 +15,8 @@ const DeckSelect: NextPage<Props> = ({ deckType, select }) => {
   return (
     <div className='select is-primary is-rounded mb-2'>
       <select data-testid='deckSelect' value={deckType} onChange={change}>
-        <option value='fibonacci'>
-          Fibonacci
-        </option>
-        <option value='sequential'>
-          1 to 10
-        </option>
+        <option value='fibonacci'>Fibonacci</option>
+        <option value='sequential'>1 to 10</option>
       </select>
     </div>
   );
--- e2e/tests/rooms/selectDeck.spec.js
+++ e2e/tests/rooms/selectDeck.spec.js
@@ -17,42 +17,42 @@ describe('rooms/selectDeck', () => {
   });
 
   test('ルームページで、デッキを変更するとき、カードの選択が解除されること', async () => {
-    await page.goto(roomUrl)
-    await page.waitForSelector('[data-testid="tableCard"]')
+    await page.goto(roomUrl);
+    await page.waitForSelector('[data-testid="tableCard"]');
 
-    const tefudaCards = await page.$$('[data-testid="tefudaCard"]')
-    await tefudaCards[0].click()
-    await page.waitForTimeout(100)
+    const tefudaCards = await page.$$('[data-testid="tefudaCard"]');
+    await tefudaCards[0].click();
+    await page.waitForTimeout(100);
 
-    let tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class')
-    expect(tefudaCardsClassName[0]).toContain('tefudaCard_selected')
+    let tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class');
+    expect(tefudaCardsClassName[0]).toContain('tefudaCard_selected');
 
-    await page.select('[data-testid="deckSelect"]', 'sequential')
+    await page.select('[data-testid="deckSelect"]', 'sequential');
 
-    tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class')
-    expect(tefudaCardsClassName[0]).not.toContain('tefudaCard_selected')
-  })
+    tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class');
+    expect(tefudaCardsClassName[0]).not.toContain('tefudaCard_selected');
+  });
 
   test('ルームページで、デッキを変更するとき、カードがオープン状態でも場がリセットされること', async () => {
-    await page.goto(roomUrl)
-    await page.waitForSelector('[data-testid="tableCard"]')
+    await page.goto(roomUrl);
+    await page.waitForSelector('[data-testid="tableCard"]');
 
-    const tefudaCards = await page.$$('[data-testid="tefudaCard"]')
-    await tefudaCards[0].click()
-    await page.click('[data-testid="openButton"]')
+    const tefudaCards = await page.$$('[data-testid="tefudaCard"]');
+    await tefudaCards[0].click();
+    await page.click('[data-testid="openButton"]');
 
-    let tableCardClassName = await getAttribute.$(page, '[data-testid="tableCard"]', 'class')
-    let tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class')
-    expect(tableCardClassName).toContain('tableCard_open')
-    expect(tefudaCardsClassName[0]).toContain('tefudaCard_selected')
+    let tableCardClassName = await getAttribute.$(page, '[data-testid="tableCard"]', 'class');
+    let tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class');
+    expect(tableCardClassName).toContain('tableCard_open');
+    expect(tefudaCardsClassName[0]).toContain('tefudaCard_selected');
 
-    await page.select('[data-testid="deckSelect"]', 'sequential')
+    await page.select('[data-testid="deckSelect"]', 'sequential');
 
-    tableCardClassName = await getAttribute.$(page, '[data-testid="tableCard"]', 'class')
-    tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class')
-    expect(tableCardClassName).toContain('tableCard_blank')
-    expect(tefudaCardsClassName[0]).not.toContain('tefudaCard_selected')
-  })
+    tableCardClassName = await getAttribute.$(page, '[data-testid="tableCard"]', 'class');
+    tefudaCardsClassName = await getAttribute.$$(page, '[data-testid="tefudaCard"]', 'class');
+    expect(tableCardClassName).toContain('tableCard_blank');
+    expect(tefudaCardsClassName[0]).not.toContain('tefudaCard_selected');
+  });
 
   test('ルームページで、「Fibonacci」を選択したとき、フィボナッチ数列のカードが並ぶこと', async () => {
     await page.goto(roomUrl);
@@ -82,20 +82,24 @@ describe('rooms/selectDeck', () => {
   });
 
   test('ルームページで、別のメンバーがデッキを選択したとき、自分の手札に反映されること', async () => {
-    await page.goto(roomUrl)
-    await page.waitForSelector('[data-testid="tableCard"]')
-    const page2 = await browser.newPage()
-    await page2.goto(roomUrl)
-    await page2.waitForSelector('[data-testid="tableCard"]')
+    await page.goto(roomUrl);
+    await page.waitForSelector('[data-testid="tableCard"]');
+    const page2 = await browser.newPage();
+    await page2.goto(roomUrl);
+    await page2.waitForSelector('[data-testid="tableCard"]');
 
-    expect(await page.$eval('[data-testid="deckSelect"]', el => el.value)).toBe('fibonacci')
-    expect(await page.$$eval('[data-testid="tefudaCard"]', els => els.map(el => el.innerText))).toEqual(['1', '2', '3', '5', '8', '13', '21', '?'])
+    expect(await page.$eval('[data-testid="deckSelect"]', (el) => el.value)).toBe('fibonacci');
+    expect(
+      await page.$$eval('[data-testid="tefudaCard"]', (els) => els.map((el) => el.innerText)),
+    ).toEqual(['1', '2', '3', '5', '8', '13', '21', '?']);
 
-    await page2.select('[data-testid="deckSelect"]', 'sequential')
+    await page2.select('[data-testid="deckSelect"]', 'sequential');
 
-    expect(await page.$eval('[data-testid="deckSelect"]', el => el.value)).toBe('sequential')
-    expect(await page.$$eval('[data-testid="tefudaCard"]', els => els.map(el => el.innerText))).toEqual(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '?'])
+    expect(await page.$eval('[data-testid="deckSelect"]', (el) => el.value)).toBe('sequential');
+    expect(
+      await page.$$eval('[data-testid="tefudaCard"]', (els) => els.map((el) => el.innerText)),
+    ).toEqual(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '?']);
 
-    await page2.close()
-  })
+    await page2.close();
+  });
 });

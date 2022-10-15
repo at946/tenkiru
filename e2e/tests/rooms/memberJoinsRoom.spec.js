d3d05fc47b32196b737c7e82b112402f4880da8d
Asato TAKAHASHI <asato730@gmail.com>
2022-10-15 10:40:47 +0900

    カードオープン状態で入室するとカードオープン状態からスタートのテストを追加

--- e2e/tests/rooms/memberJoinsRoom.spec.js
+++ e2e/tests/rooms/memberJoinsRoom.spec.js
@@ -29,4 +29,22 @@ describe('rooms/memberJoinsRoom', () => {
     expect(await page2.$$eval('[data-testid="tableCard"]', (els) => els.length)).toBe(1);
     await page2.close();
   });
+
+  test('ルームページで、カードがオープンした部屋に入室したとき、カードがオープンな状態からスタートすること', async () => {
+    await page.goto(roomUrl)
+    await page.waitForSelector('[data-testid="tableCard"]')
+
+    const tefudaCards = await page.$$('[data-testid="tefudaCard"]')
+    await tefudaCards[0].click()
+    await page.click('[data-testid="openButton"]')
+
+    const page2 = await browser.newPage()
+    await page2.goto(roomUrl)
+    await page2.waitForSelector('[data-testid="tableCard"]')
+
+    expect(await page2.$('[data-testid="openButton"]')).toBeNull()
+    expect(await page2.$('[data-testid="replayButton"]')).not.toBeNull()
+    const tefudaCards2ClassName = await getAttribute.$$(page2, '[data-testid="tefudaCard"]', 'class')
+    expect(tefudaCards2ClassName[0]).toContain('tefudaCard_disabled')
+  })
 });

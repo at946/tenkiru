0f24b04340175d75cb818113c3f4e5d726fe72ba
Lint Action <lint-action@samuelmeuli.com>
2022-10-29 00:50:22 +0000

    Fix code style issues with Prettier

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -18,16 +18,16 @@ const Header: NextPage = () => {
         <div className='navbar-menu'>
           <div className='navbar-end'>
             <a
-              href="https://www.buymeacoffee.com/at946"
-              className="navbar-item"
-              target="_blank"
-              rel="noreferrer"
-              data-testid="buymeacoffee"
-              >
+              href='https://www.buymeacoffee.com/at946'
+              className='navbar-item'
+              target='_blank'
+              rel='noreferrer'
+              data-testid='buymeacoffee'
+            >
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <img
-                src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-                alt="Buy me a coffee"
+                src='https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png'
+                alt='Buy me a coffee'
               />
             </a>
           </div>

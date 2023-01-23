93a6ed9143800e25ca0a27fafd3db0f3b074911c
Asato TAKAHASHI <asato730@gmail.com>
2023-01-23 22:41:27 +0900

    footer/links test moves to playwright

---
+++ playwright/tests/footer/links.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import urls from '../../helpers/urls';
+import usersJoinRoom from '../../helpers/usersJoinRoom';
+
+test('トップページで、フッターのコピーライトを選択したとき、開発者のTwitterプロフィールページへ遷移すること', async ({ page }) => {
+  await page.goto(urls.top)
+  const copyright = page.locator('data-testid=copyright')
+  await expect(copyright).toHaveText('@asato')
+  await expect(copyright).toHaveAttribute('href', 'https://twitter.com/at_946')
+  await expect(copyright).toHaveAttribute('target', '_blank')
+});
+
+test('トップページで、フッターの利用規約を選択したとき、利用規約ページへ遷移すること', async ({ page }) => {
+  await page.goto(urls.top)
+  await expect(page).toHaveURL(urls.top)
+  await page.locator('data-testid=link_to_tos').click()
+  await expect(page).toHaveURL(urls.tos)
+});
+
+test('トップページで、フッターのプライバシーポリシーを選択したとき、プライバシーポリシーページへ遷移すること', async ({ page }) => {
+  await page.goto(urls.top)
+  await expect(page).toHaveURL(urls.top)
+  await page.locator('data-testid=link_to_pp').click()
+  await expect(page).toHaveURL(urls.pp)
+});
+
+test('トップページで、フッターのお問い合わせを選択したとき、開発者のTwitterプロフィールページへ遷移すること', async ({ page }) => {
+  await page.goto(urls.top)
+  const copyright = page.locator('data-testid=link_to_inquiry')
+  await expect(copyright).toHaveAttribute('href', 'https://twitter.com/at_946')
+  await expect(copyright).toHaveAttribute('target', '_blank')
+});

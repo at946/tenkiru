2d112153be3937d56379111b9eb1dcff8d0020f7
Asato TAKAHASHI <asato730@gmail.com>
2023-04-18 21:52:37 +0900

    指名したとき指名完了メッセージを表示

--- app/rooms/[roomId]/components/table/nominateButton.tsx
+++ app/rooms/[roomId]/components/table/nominateButton.tsx
@@ -1,16 +1,28 @@
 import { NextPage } from 'next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { event } from '@/lib/gtag';
+import { toast } from 'bulma-toast';
 
 interface Props {
   nominate: () => void;
 }
 
 const NominateButton: NextPage<Props> = ({ nominate }) => {
+  const onClick = (): void => {
+    nominate();
+    toast({
+      message: '指名しました！',
+      type: 'is-success',
+      position: 'top-center',
+    });
+    event({ action: 'nominate', category: 'engagement', label: '' });
+  };
+
   return (
     <button
       className='button is-small is-primary is-inverted'
-      onClick={nominate}
+      onClick={onClick}
       data-testid='nominateButton'
     >
       <FontAwesomeIcon icon={faComment} className='mr-1' />

5b5e43aebedd4348fa53891f9cf5bb85d8cf9549
Asato TAKAHASHI <asato730@gmail.com>
2023-04-23 23:52:19 +0900

    refactor member-selects-card test with page object model

--- playwright/tests/rooms/memberSelectsCard.spec.ts
+++ playwright/tests/rooms/memberSelectsCard.spec.ts
@@ -1,21 +1,30 @@
 import { test, expect } from '@playwright/test';
-import urls from '../../helpers/urls';
-import usersJoinRoom from '../../helpers/usersJoinRoom';
+import RoomPage from '../../models/room-page';
+import createRoomId from '../../helpers/createRoomId';
 
 test('ルームページで、他のメンバーがカードを選択したとき、カードが場に伏せて置かれること', async ({
   context,
 }) => {
-  const roomURL = urls.room();
-  const [page1, page2] = await usersJoinRoom(context, roomURL, 2);
+  // Given
+  const roomId: string = createRoomId();
+  const roomPage1: RoomPage = new RoomPage(await context.newPage());
+  const roomPage2: RoomPage = new RoomPage(await context.newPage());
+  await roomPage1.goto(roomId);
+  await roomPage2.goto(roomId);
+  await roomPage1.selectCard('0');
+  await expect(roomPage1.tableCards).toHaveCount(2);
+  await expect(roomPage1.faceDownTableCards).toHaveCount(1);
+  await expect(roomPage1.blankTableCards).toHaveCount(1);
+  await expect(roomPage2.tableCards).toHaveCount(2);
+  await expect(roomPage2.faceDownTableCards).toHaveCount(1);
+  await expect(roomPage2.blankTableCards).toHaveCount(1);
 
-  const tableCards = page1.locator('data-testid=tableCard');
-  await expect(tableCards).toHaveCount(2);
-  await expect(tableCards.nth(0)).toHaveClass(/tableCard_blank/);
-  await expect(tableCards.nth(1)).toHaveClass(/tableCard_blank/);
+  // When
+  await roomPage2.selectCard('0');
 
-  await page2.locator('data-testid=tefudaCard').nth(0).click();
-
-  await expect(tableCards).toHaveCount(2);
-  await expect(tableCards.nth(0)).toHaveClass(/tableCard_close/);
-  await expect(tableCards.nth(1)).toHaveClass(/tableCard_blank/);
+  // Then
+  await expect(roomPage1.tableCards).toHaveCount(2);
+  await expect(roomPage1.faceDownTableCards).toHaveCount(2);
+  await expect(roomPage2.tableCards).toHaveCount(2);
+  await expect(roomPage2.faceDownTableCards).toHaveCount(2);
 });

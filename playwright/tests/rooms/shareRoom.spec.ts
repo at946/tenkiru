cd08adf1182032e28b629f56e0f50edcf255b7e3
Asato TAKAHASHI <asato730@gmail.com>
2023-04-26 23:14:33 +0900

    refactor share-room test with page object model

--- playwright/tests/rooms/shareRoom.spec.ts
+++ playwright/tests/rooms/shareRoom.spec.ts
@@ -1,43 +1,34 @@
-import { test, expect } from '@playwright/test';
-import urls from '../../helpers/urls';
-import usersJoinRoom from '../../helpers/usersJoinRoom';
+import { test, expect, Page } from '@playwright/test';
+import RoomPage from '../../models/room-page';
+import createRoomId from '../../helpers/createRoomId';
 
-test('ルームページで、ルームIDを知れること', async ({ context }) => {
-  const [page] = await usersJoinRoom(context, 'rooms/sampleroom', 1);
+test('ルームページで、部屋番号を知れること', async ({ page }) => {
+  // Given
+  const roomId: string = createRoomId();
+  const roomPage: RoomPage = new RoomPage(page);
 
-  await expect(page.locator('data-testid=roomId')).toHaveText('sampleroom');
+  // When
+  await roomPage.goto(roomId);
+
+  // Then
+  await expect(roomPage.roomIdLink).toHaveText(`部屋番号：${roomId}`);
 });
 
-// test('ルームページで、ルームIDを選択したとき、URLをクリップボードにコピーできること', async ({ page }) => {
-//   // TODO: https://github.com/puppeteer/puppeteer/issues/8692 に進展あったらやるかもね
-//   // await browser.defaultBrowserContext().overridePermissions('http://gateway.docker.internal', ['clipboard-read'])
-//   // expect(await page1.evaluate(() => navigator.clipboard.readText())).toBe('')
-//   // const page1 = await browser.newPage()
-//   // await page1.goto(roomUrl)
-//   // await page1.click('[data-testid="roomId"]')
-//   // expect(await page1.evaluate(() => navigator.clipboard.readText())).toBe(roomUrl)
-//   // const otherRoomUrl = urls.room()
-//   // await page2 = await browser.newPage()
-//   // await page2.goto(otherRoomUrl)
-//   // await page2.click('[data-testid="roomIdShareIcon"]')
-//   // expect(await page2.evaluate(() => navigator.clipboard.readText())).toBe(otherRoomUrl)
+// test('ルームページで、ルームIDを選択したとき、URLをクリップボードにコピーできること', async ({
+//   context,
+// }) => {
+//   // Given
+//   await context.grantPermissions(['clipboard-write', 'clipboard-read']);
+//   const page: Page = await context.newPage();
+//   const roomId: string = createRoomId();
+//   const roomPage: RoomPage = new RoomPage(page);
+//   await roomPage.goto(roomId);
+
+//   // When
+//   await roomPage.copyRoomUrl();
+
+//   // Then
+//   await expect(page.getByText('クリップボードにこの部屋のURLをコピーしました。')).toBeVisible();
+//   const clipboardText: string = await page.evaluate('navigator.clipboard.read()');
+//   await expect(clipboardText).toBe(`http://localhost:3000/rooms/${roomId}`);
 // });
-
-test('シェアされたルームページのURLに直接アクセスしたとき、ルームに入れること', async ({
-  page,
-}) => {
-  const roomURL1 = urls.room();
-  const roomURL2 = urls.room();
-  const tableCards = page.locator('data-testid=tableCard');
-  const roomId = page.locator('data-testid=roomId');
-
-  await page.goto('/rooms/room1');
-  await expect(tableCards).toHaveCount(1);
-  await expect(page).toHaveURL('/rooms/room1');
-  await expect(roomId).toHaveText('room1');
-
-  await page.goto('/rooms/room2');
-  await expect(tableCards).toHaveCount(1);
-  await expect(page).toHaveURL('/rooms/room2');
-  await expect(roomId).toHaveText('room2');
-});

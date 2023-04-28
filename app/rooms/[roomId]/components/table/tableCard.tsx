96f3b44c3a6b2d8e24be408c2be07bc64b6e69a3
Asato TAKAHASHI <asato730@gmail.com>
2023-04-28 22:23:38 +0900

    type errorの解消

--- app/rooms/[roomId]/components/table/tableCard.tsx
+++ app/rooms/[roomId]/components/table/tableCard.tsx
@@ -2,20 +2,21 @@ import { NextPage } from 'next';
 import Card from '../card';
 import styles from './tableCard.module.scss';
 import { Card as IFCard } from '@/interfaces/card';
+import { TableCardStatus } from '@/interfaces/tableCardStatus';
 import { useAppSelector } from '@/store/hooks';
 
 interface Props {
   value: IFCard;
-  status: string;
+  status: TableCardStatus;
 }
 
 const TableCard: NextPage<Props> = ({ value, status }) => {
-  if (status === 'blank') {
-    return <Card additionalClassName={styles.blank} ariaLabel='未選択のテーブルカード' />;
+  if (status === 'faceUp') {
+    return <Card value={value} ariaLabel={`めくられたテーブルカード ${value}`} />;
   } else if (status === 'faceDown') {
     return <Card additionalClassName={styles['face-down']} ariaLabel='伏せられたテーブルカード' />;
-  } else if (status === 'faceUp') {
-    return <Card value={value} ariaLabel={`めくられたテーブルカード ${value}`} />;
+  } else { // blank
+    return <Card additionalClassName={styles.blank} ariaLabel='未選択のテーブルカード' />;
   }
 };
 
---
+++ interfaces/tableCardStatus.ts
@@ -0,0 +1,1 @@
+export type TableCardStatus = 'blank' | 'faceDown' | 'faceUp';

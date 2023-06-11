399b5ea0df1f4387234b688d493bf6c75de09d4a
Asato TAKAHASHI <asato730@gmail.com>
2023-06-11 22:02:27 +0900

    remove useless import from CreateRoomButton

--- app/components/CreateRoomButton.tsx
+++ app/components/CreateRoomButton.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import { faDoorClosed, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NextPage } from 'next';
-import Link from 'next/link';
-import { useState } from 'react';
-import { event } from '@/lib/gtag';
-import { v4 } from 'uuid';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { v4 } from 'uuid';
+import { faDoorClosed, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+import { event } from '@/lib/gtag';
+
 import Button from './common/Button';
 
 const CreateRoomButton: NextPage = () => {
+  const router = useRouter();
   const [isHovering, setIsHovering] = useState<boolean>(false);
   const roomId: string = v4();
-  const router = useRouter();
+
   const click = (): void => {
     event({ action: 'create_room', category: 'engagement', label: '' });
     router.push(`/rooms/${encodeURIComponent(v4())}`);

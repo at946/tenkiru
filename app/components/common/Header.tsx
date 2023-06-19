b242a875bbd0a18bad9c11602af43ccb5ecea8ad
Asato TAKAHASHI <asato730@gmail.com>
2023-06-19 16:50:08 +0900

    twitter share menu

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMugHot } from '@fortawesome/free-solid-svg-icons';
+import { faMugSaucer, faShuffle } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Header: NextPage = () => {
+  const twitterShareText: string =
+    '#テンキル\n' +
+    'チームでわいわいプランニングポーカーアプリ\n' +
+    process.env.NEXT_PUBLIC_BASE_URL;
+
   return (
     <nav role='navigation' aria-label='main navigation'>
       <div className='container mx-auto p-5'>
@@ -13,6 +19,7 @@ const Header: NextPage = () => {
               href='/'
               className='px-2 font-bold text-purple-600 hover:underline focus:underline'
             >
+              <FontAwesomeIcon icon={faShuffle} className='mr-2' />
               Tenkir
             </Link>
           </div>
@@ -21,10 +28,19 @@ const Header: NextPage = () => {
               href='https://note.com/_at_946/n/nb84babf02d87'
               className='px-2 hover:text-purple-600 focus:text-purple-600'
               target='_blank'
-              rel='noreferrer'
+              rel='noreferrer nofollow noopener'
             >
-              <FontAwesomeIcon icon={faMugHot} className='mr-2' />
-              <span>開発者を支援</span>
+              <FontAwesomeIcon icon={faMugSaucer} className='mr-1' />
+              <span className='hidden md:inline'>コーヒーで支援</span>
+            </a>
+            <a
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(twitterShareText)}`}
+              className='px-2 hover:text-purple-600 focus:text-purple-600'
+              target='_blank'
+              rel='noreferrer nofollow noopener'
+            >
+              <FontAwesomeIcon icon={faTwitter} className='mr-1' />
+              <span className='hidden md:inline'>シェアで支援</span>
             </a>
           </div>
         </div>

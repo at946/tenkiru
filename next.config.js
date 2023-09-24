94ed6b54e083394bfdcadf731be282be350413b8
Asato TAKAHASHI <asato730@gmail.com>
2023-09-24 21:38:59 +0900

    upgrade next to 13.5.2

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const path = require('path');
 module.exports = {
   reactStrictMode: false,
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;

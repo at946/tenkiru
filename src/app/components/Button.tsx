7aed724e7777e8e8d155b9449f205a17a7743797
Asato TAKAHASHI <asato730@gmail.com>
2024-04-15 23:28:57 +0900

    Replaced focus with focus-visible in Button to remove focus style when hover out after clicking it

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -34,17 +34,18 @@ const Button: NextPage<Props> = ({
   return (
     <button
       className={clsx(
-        'inline-flex items-center gap-1 rounded-full border-2 px-4 py-2 drop-shadow-md enabled:hover:drop-shadow-lg enabled:focus:drop-shadow-lg disabled:cursor-not-allowed disabled:opacity-50',
+        'inline-flex items-center gap-1 rounded-full border-2 px-4 py-2 drop-shadow-md enabled:hover:drop-shadow-lg enabled:focus-visible:drop-shadow-lg disabled:cursor-not-allowed disabled:opacity-50',
         color === 'primary' && 'border-primary',
         color === 'secondary' && 'border-secondary',
-        isOutlined && 'bg-background enabled:hover:text-background enabled:focus:text-background',
+        isOutlined &&
+          'bg-background enabled:hover:text-background enabled:focus-visible:text-background',
         isOutlined &&
           color === 'primary' &&
-          'text-primary enabled:hover:bg-primary enabled:focus:bg-primary',
+          'text-primary enabled:hover:bg-primary enabled:focus-visible:bg-primary',
         isOutlined &&
           color === 'secondary' &&
-          'text-secondary enabled:hover:bg-secondary enabled:focus:bg-secondary',
-        !isOutlined && 'text-white enabled:hover:opacity-75 enabled:focus:opacity-75',
+          'text-secondary enabled:hover:bg-secondary enabled:focus-visible:bg-secondary',
+        !isOutlined && 'text-white enabled:hover:opacity-75 enabled:focus-visible:opacity-75',
         !isOutlined && color === 'primary' && 'bg-primary',
         !isOutlined && color === 'secondary' && 'bg-secondary',
         className,

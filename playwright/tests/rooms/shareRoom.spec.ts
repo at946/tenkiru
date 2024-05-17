394abec25f7389385631aefd57ab8db71aaec767
Asato TAKAHASHI <asato730@gmail.com>
2024-05-17 22:11:27 +0900

    Translated share room spec into English

--- playwright/tests/rooms/shareRoom.spec.ts
+++ playwright/tests/rooms/shareRoom.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test';
-import createRoomId from '../../helpers/createRoomId';
-import RoomPage from '../../models/room-page';
+import createRoomId from '@pw/helpers/createRoomId';
+import RoomPage from '@pw/models/room-page';
 
-test('ルームページで、部屋番号を知れること', async ({ page }) => {
+test('On the room page, users should be able to get the room ID.', async ({ page }) => {
   // Given
   const roomId: string = createRoomId();
   const roomPage: RoomPage = new RoomPage(page);
@@ -14,11 +14,9 @@ test('ルームページで、部屋番号を知れること', async ({ page }) 
   await expect(roomPage.roomIdLink).toHaveText(`Room ID:${roomId}`);
 });
 
-// test('ルームページで、ルームIDを選択したとき、URLをクリップボードにコピーできること', async ({
-//   context,
-// }) => {
+// test('ルームページで、ルームIDを選択したとき、URLをクリップボードにコピーできること', async ({ context }) => {
 //   // Given
-//   await context.grantPermissions(['clipboard-write', 'clipboard-read']);
+//   await context.grantPermissions(['clipboard-read']);
 //   const page: Page = await context.newPage();
 //   const roomId: string = createRoomId();
 //   const roomPage: RoomPage = new RoomPage(page);
@@ -28,7 +26,7 @@ test('ルームページで、部屋番号を知れること', async ({ page }) 
 //   await roomPage.copyRoomUrl();
 
 //   // Then
-//   await expect(page.getByText('クリップボードにこの部屋のURLをコピーしました。')).toBeVisible();
-//   const clipboardText: string = await page.evaluate('navigator.clipboard.read()');
-//   await expect(clipboardText).toBe(`http://localhost:3000/rooms/${roomId}`);
+//   // await expect(page.getByText('Copied this Room URL')).toBeVisible();
+//   const clipboardText: string = await page.evaluate('navigator.clipboard.readText()');
+//   // await expect(clipboardText).toBe(`http://localhost:3000/rooms/${roomId}`);
 // });

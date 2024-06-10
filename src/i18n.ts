ddf0532d2d3975dc0e1c5e7704759373eb1f7812
Asato TAKAHASHI <asato730@gmail.com>
2024-06-11 08:55:26 +0900

    Fixed some types' source changed by major version up

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,8 +1,8 @@
-import type { AllLocales, LocalePrefix } from 'next-intl/dist/types/src/shared/types';
+import type { LocalePrefix, Locales } from 'next-intl/dist/types/src/routing/types';
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 
-export const locales: AllLocales = ['en', 'ja'];
+export const locales: Locales = ['en', 'ja'];
 export type TLocales = 'en' | 'ja';
 export const localePrefix: LocalePrefix = 'always';
 

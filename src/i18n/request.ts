a73082091208e002c98d6a79c90cadfd20a035cb
at946 <asato730@gmail.com>
2025-01-25 13:09:03 +0900

    fixed by biome

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,14 +1,14 @@
-import { routing, type TLocales } from "@/i18n/routing";
-import { getRequestConfig } from "next-intl/server";
+import { type TLocales, routing } from '@/i18n/routing';
+import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
   if (!locale || !routing.locales.includes(locale as TLocales)) {
     locale = routing.defaultLocale;
   }
-  
+
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default,
-  }
-})
+  };
+});

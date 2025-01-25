78025fa501da83138f9604906538ce1785643fe0
at946 <asato730@gmail.com>
2025-01-25 13:02:00 +0900

    change the way to get params data in room page into async

--- src/app/[locale]/rooms/[roomId]/page.tsx
+++ src/app/[locale]/rooms/[roomId]/page.tsx
@@ -1,22 +1,23 @@
 import MyToaster from '@/app/[locale]/components/common/MyToaster';
 import RoomInfo from '@/app/[locale]/rooms/[roomId]/components/RoomInfo';
 import { pick } from 'lodash';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
 import RoomPage from './RoomPage';
 
-interface Props {
-  params: {
-    roomId: string;
-  };
-}
+type Params = Promise<{
+  roomId: string;
+}>;
 
-export default function Page(props: Props) {
-  const messages = useMessages();
+export default async function Page(props: { params: Params }) {
+  const params = await props.params;
+  const messages = await getMessages();
+
   return (
     <div className='container mx-auto mt-5 mb-10 px-5 text-center'>
-      <RoomInfo roomId={props.params.roomId} className='mb-5' />
+      <RoomInfo roomId={params.roomId} className='mb-5' />
       <NextIntlClientProvider messages={pick(messages, 'Room')}>
-        <RoomPage roomId={props.params.roomId} />
+        <RoomPage roomId={params.roomId} />
       </NextIntlClientProvider>
       <MyToaster />
     </div>

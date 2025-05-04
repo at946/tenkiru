ae238b0c4822fa6a3150b3ded50e92a8587fb069
at946 <asato730@gmail.com>
2025-05-04 22:04:00 +0900

    Changed the three steps to start on the top page from flexbox to grid

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -13,18 +13,22 @@ export default function Page() {
         <CreateRoomButton name={t('Create a room')} />
       </div>
 
-      <div className='flex flex-col items-center justify-center gap-2 md:flex-row md:items-stretch'>
-        <div className='how-to-use-box'>
+      <div className='grid grid-cols-none items-center gap-5 md:grid-cols-10'>
+        <div className='how-to-use-box md:col-span-2 md:col-start-2'>
           <span className='icon-[fa6-solid--door-open] text-5xl' />
           <p className='break-auto-phrase mt-2'>{t('Create a room for planning poker')}</p>
         </div>
-        <span className='icon-[fa6-solid--angle-down] md:icon-[fa6-solid--angle-right] my-auto text-3xl' />
-        <div className='how-to-use-box'>
+        <div>
+          <span className='icon-[fa6-solid--angle-down] md:icon-[fa6-solid--angle-right] col-span-1 text-3xl' />
+        </div>
+        <div className='how-to-use-box md:col-span-2'>
           <span className='icon-[mdi--share-all-outline] text-5xl' />
           <p className='break-auto-phrase mt-2'>{t('Share the room URL with your team')}</p>
         </div>
-        <span className='icon-[fa6-solid--angle-down] md:icon-[fa6-solid--angle-right] my-auto text-3xl' />
-        <div className='how-to-use-box'>
+        <div>
+          <span className='icon-[fa6-solid--angle-down] md:icon-[fa6-solid--angle-right] col-span-1 text-3xl' />
+        </div>
+        <div className='how-to-use-box md:col-span-2'>
           <span className='icon-[mdi--cards-playing-outline] text-5xl' />
           <p className='break-auto-phrase mt-2'>{t('Enjoy planning poker together')}</p>
         </div>

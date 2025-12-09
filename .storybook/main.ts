826c2e2fd1413da4690cd8515dafb1ed775e5f20
at946 <asato730@gmail.com>
2025-12-09 13:06:15 +0900

    Remove storybook-dark-mode addon from config
    The storybook-dark-mode addon has been removed from the Storybook configuration. This may be to simplify the addon setup or because dark mode support is no longer needed.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
   stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx)'],
 
-  addons: ['@storybook/addon-links', '@storybook/addon-a11y', 'storybook-dark-mode', '@storybook/addon-docs'],
+  addons: ['@storybook/addon-links', '@storybook/addon-a11y', '@storybook/addon-docs'],
 
   framework: {
     name: '@storybook/nextjs',

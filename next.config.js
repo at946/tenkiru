42bf3a7b806473ccc5bd6295c6203c4846624a97
at946 <asato730@gmail.com>
2026-04-12 22:48:06 +0900

    fix: update tunnelRoute in Sentry configuration and adjust proxy matcher

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ module.exports = withSentryConfig(module.exports, {
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
   // side errors will fail.
-  tunnelRoute: '/monitoring',
+  tunnelRoute: '/sentry-tunnel',
 
   webpack: {
     // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
--- src/proxy.ts
+++ src/proxy.ts
@@ -4,5 +4,5 @@ import { routing } from '@/i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|audio/|ads.txt|opengraph-image.jpg).*)'],
+  matcher: ['/((?!sentry-tunnel|api|_next/static|_next/image|favicon.ico|audio/|ads.txt|opengraph-image.jpg).*)'],
 };

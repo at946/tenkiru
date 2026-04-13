a009be57767500d42c313ee1b046a7eb54bc4e07
at946 <asato730@gmail.com>
2026-04-13 16:52:05 +0900

    fix: update Sentry initialization to use NEXT_PUBLIC_SENTRY_ENABLED environment variable

--- environment.d.ts
+++ environment.d.ts
@@ -5,6 +5,7 @@ declare global {
       NEXT_PUBLIC_BASE_URL: string;
       NEXT_PUBLIC_GA_ID?: string;
       NEXT_PUBLIC_GOOGLE_ADSENSE_ID?: string;
+      NEXT_PUBLIC_SENTRY_ENABLED?: string;
     }
   }
 }
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  enabled: process.env.NODE_ENV === 'production',
+  enabled: process.env.NEXT_PUBLIC_SENTRY_ENABLED === 'true',
 
   dsn: 'https://d1a492125e035fa653c1c2b6645f70c1@o4511196392783872.ingest.us.sentry.io/4511196393111552',
 

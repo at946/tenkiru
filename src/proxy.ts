9de66098095dfc2b62b30f3e50ef0a4149d22ada
at946 <52263024+at946@users.noreply.github.com>
2026-05-12 23:14:20 +0900

    proxy.ts を更新

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,6 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 import { routing } from '@/i18n/routing';
 
 const proxy = createMiddleware(routing);
+
 export { proxy };
 
 export const config = {

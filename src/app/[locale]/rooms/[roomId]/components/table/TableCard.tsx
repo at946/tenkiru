1e9fcaa0d88cc03dd02d738fdc861fc406a85d51
at946 <52263024+at946@users.noreply.github.com>
2026-06-28 21:08:05 +0900

    Separate the table card place's style from the table card's style in TableCard.tsx

--- src/app/[locale]/rooms/[roomId]/components/table/TableCard.tsx
+++ src/app/[locale]/rooms/[roomId]/components/table/TableCard.tsx
@@ -14,25 +14,32 @@ const TableCard: NextPage<Props> = ({ value, isOpen = false }) => {
   const isBlank: boolean = value === null;
 
   return (
-    <button
-      type='button'
-      className={clsx(
-        'flex aspect-card w-24 items-center justify-center rounded-md border-2 border-text font-bold text-2xl shadow-md',
-        isBlank && 'border-dashed',
-        !isBlank && !isOpen && 'bg-secondary',
-        !isBlank && isOpen && 'bg-background text-text transition-all duration-1000 ease-out',
+    <div className='relative aspect-card w-24'>
+      <div
+        className='absolute inset-0 rounded-md border-2 border-dashed border-text'
+        aria-hidden='true'
+        aria-label={isBlank && t('Unselected table card')}
+      />
+
+      {!isBlank && (
+        <button
+          type='button'
+          className={clsx(
+            'absolute inset-0 flex aspect-card w-24 items-center justify-center rounded-md border-2 border-text font-bold text-2xl shadow-md',
+            !isOpen && 'bg-secondary',
+            isOpen && 'bg-background text-text transition-all duration-1000 ease-out',
+          )}
+          style={!isBlank && !isOpen ? { transform: 'rotateY(-180deg)' } : {}}
+          aria-label={
+            isOpen
+              ? `${t('Face-up table card')} ${value}`
+              : t('Face-down table card')
+          }
+        >
+          {isOpen && value}
+        </button>
       )}
-      style={!isBlank && !isOpen ? { transform: 'rotateY(-180deg)' } : {}}
-      aria-label={
-        isBlank
-          ? t('Unselected table card')
-          : isOpen
-            ? `${t('Face-up table card')} ${value}`
-            : t('Face-down table card')
-      }
-    >
-      {isOpen && value}
-    </button>
+    </div>
   );
 };
 

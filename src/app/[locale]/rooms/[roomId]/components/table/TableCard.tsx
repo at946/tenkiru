dda78aafce956510188374fad802ec172aaaa469
at946 <52263024+at946@users.noreply.github.com>
2026-06-28 22:06:05 +0900

    TableCard.tsx を更新

--- src/app/[locale]/rooms/[roomId]/components/table/TableCard.tsx
+++ src/app/[locale]/rooms/[roomId]/components/table/TableCard.tsx
@@ -14,30 +14,34 @@ const TableCard: NextPage<Props> = ({ value, isOpen = false }) => {
   const isBlank: boolean = value === null;
 
   return (
-    <div className='relative aspect-card w-24'>
+    <div
+      className='relative aspect-card w-24'
+      role='img'
+      aria-label={
+        isBlank
+          ? t('Unselected table card')
+          : isOpen
+            ? `${t('Face-up table card')} ${value}`
+            : t('Face-down table card')
+      }
+    >
       <div
-        className='absolute inset-0 rounded-md border-2 border-dashed border-text'
+        className='absolute inset-0 rounded-md border-2 border-text border-dashed'
         aria-hidden='true'
-        aria-label={isBlank && t('Unselected table card')}
       />
 
       {!isBlank && (
-        <button
-          type='button'
+        <div
           className={clsx(
             'absolute inset-0 flex aspect-card w-24 items-center justify-center rounded-md border-2 border-text font-bold text-2xl shadow-md',
             !isOpen && 'bg-secondary',
             isOpen && 'bg-background text-text transition-all duration-1000 ease-out',
           )}
           style={!isBlank && !isOpen ? { transform: 'rotateY(-180deg)' } : {}}
-          aria-label={
-            isOpen
-              ? `${t('Face-up table card')} ${value}`
-              : t('Face-down table card')
-          }
+          aria-hidden='true'
         >
           {isOpen && value}
-        </button>
+        </div>
       )}
     </div>
   );

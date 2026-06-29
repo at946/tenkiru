5b9bd91bbfcb367f6c4d6a18db68d29590b7ccb4
at946 <52263024+at946@users.noreply.github.com>
2026-06-29 20:17:45 +0900

    PokerCardSlot.tsx を作成

---
+++ src/app/[locale]/rooms/[roomId]/components/poker-card/PokerCardSlot.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  children?: React.ReactNode;
+}
+
+const PokerCardSlot = ({ children }: Props) => {
+  return (
+    <div className='relative aspect-card w-24'>
+      <div
+        aria-hidden='true'
+        className='absolute inset-0 rounded-xl border border-white/30'
+      />
+
+      <div className='absolute inset-1 flex items-center justify-center'>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default PokerCardSlot;

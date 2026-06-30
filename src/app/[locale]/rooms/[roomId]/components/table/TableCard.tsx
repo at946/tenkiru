73aa6958b6c54a7a089861fa9b539f2de1642a1e
at946 <asato730@gmail.com>
2026-06-30 22:37:25 +0900

    fix typescript error

--- src/app/[locale]/rooms/[roomId]/components/table/TableCard.tsx
+++ src/app/[locale]/rooms/[roomId]/components/table/TableCard.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next';
-import type { IFTableCardValue } from '@/interfaces/tableCardValue';
 import PokerCardBack from '../poker-card/PokerCardBack';
 import PokerCardFront from '../poker-card/PokerCardFront';
 import TableCardSlot from './TableCardSlot';
 
 interface Props {
-  value: IFTableCardValue;
+  value: number | '?';
   isOpen?: boolean;
 }
 
--- src/app/[locale]/rooms/[roomId]/components/table/TableCardSlot.tsx
+++ src/app/[locale]/rooms/[roomId]/components/table/TableCardSlot.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import type { ComponentPropsWithoutRef } from 'react';
 
 type Props = ComponentPropsWithoutRef<'div'>;
 

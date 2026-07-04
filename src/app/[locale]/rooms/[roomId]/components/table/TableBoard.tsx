a7fcde85866a6f10480102fdedb0124419ecd0cb
at946 <52263024+at946@users.noreply.github.com>
2026-07-05 00:20:41 +0900

    Created the TableBoard.tsx to split the style responsibility from Table.tsx

---
+++ src/app/[locale]/rooms/[roomId]/components/table/TableBoard.tsx
@@ -0,0 +1,22 @@
+import clsx from 'clsx';
+import type { ComponentPropsWithoutRef } from 'react';
+import { useTranslations } from 'next-intl';
+
+type Props = ComponentPropsWithoutRef<'div'>;
+
+const TableBoard = ({ children, className, ...props}: Props) => {
+  const t = useTranslations('Room.Table');
+
+  return (
+    <div
+      {...props}
+      role='img'
+      aria-label={t('Table')}
+      className={clsx('rounded-sm bg-green-400 py-5 text-center shadow-md', className)}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default TableBoard;

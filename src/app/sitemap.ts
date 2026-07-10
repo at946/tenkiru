ee95b32d09c2d0d92f0e6e19624adb078bccb420
at946 <52263024+at946@users.noreply.github.com>
2026-07-11 05:06:35 +0900

    refactor sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,28 +1,26 @@
 import type { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL as string;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  if (!baseUrl) {
+    throw new Error('NEXT_PUBLIC_BASE_URL is not set');
+  }
+
+  const lastModified = new Date();
   const locales = ['en', 'ja'];
   const pages = ['', '/pp', '/tos'];
 
-  const sitemapEntries: MetadataRoute.Sitemap = [];
-
-  for (const page of pages) {
-    for (const locale of locales) {
-      sitemapEntries.push({
-        url: `${baseUrl}/${locale}${page}`,
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: page === '' ? 1.0 : 0.8,
-        alternates: {
-          languages: {
-            en: `${baseUrl}/en${page}`,
-            ja: `${baseUrl}/ja${page}`,
-          },
-        },
-      });
-    }
-  }
-
-  return sitemapEntries;
+  return pages.flatMap((page) =>
+    locales.map((locale) => ({
+      url: `${baseUrl}/${locale}${page}`,
+      lastModified: lastModified,
+      changeFrequency: 'monthly',
+      priority: page === '' ? 1.0 : 0.8,
+      alternates: {
+        languages: Object.fromEntried(
+          locales.map((locale) => [locale, `${baseUrl}/${locale}${page}`]),
+        )
+      },
+    }));
+  )
 }

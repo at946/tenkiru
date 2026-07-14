bb1974bc87adac04bec36215c417a4c9cd812a9f
at946 <asato730@gmail.com>
2026-07-14 22:59:40 +0900

    refactor: remove ariaLabel prop from LocaleSwitcher and use translation for accessibility

--- src/app/[locale]/components/common/LocaleSwitcher.tsx
+++ src/app/[locale]/components/common/LocaleSwitcher.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import type { NextPage } from 'next';
+import { useTranslations } from 'next-intl';
 import Select from '@/app/[locale]/components/common/Select';
 import { routing, usePathname, useRouter } from '@/i18n/routing';
 
 interface Props {
   currentLocale: string;
-  ariaLabel: string;
 }
 
-const LocaleSwitcher: NextPage<Props> = ({ currentLocale, ariaLabel }) => {
+const LocaleSwitcher = ({ currentLocale }: Props) => {
   const router = useRouter();
   const pathname: string = usePathname();
+  const t = useTranslations('Header');
 
   const switchLolale = (locale: string): void => {
     router.replace(pathname, { locale: locale });
@@ -22,7 +22,8 @@ const LocaleSwitcher: NextPage<Props> = ({ currentLocale, ariaLabel }) => {
       label={<span className='icon-[ic--baseline-language] mr-1 text-2xl' />}
       value={currentLocale}
       onChange={(e) => switchLolale(e.target.value)}
-      aria-label={ariaLabel}
+      aria-label={t('Switch language')}
+      className='uppercase'
     >
       {routing.locales.map((locale) => (
         <option value={locale} key={locale}>

f72b41a31d45b33de28d98c36092beddfecd996f
at946 <asato730@gmail.com>
2026-07-14 21:36:02 +0900

    refactor: simplify RoomInfo component layout and improve styling

--- src/app/[locale]/rooms/[roomId]/components/RoomInfo.tsx
+++ src/app/[locale]/rooms/[roomId]/components/RoomInfo.tsx
@@ -15,11 +15,10 @@ const RoomInfo = ({ className, ...props }: Props) => {
         copiedText={`${process.env.NEXT_PUBLIC_BASE_URL}/rooms/${room.id}`}
         messageOnSuccess={t('Copied this Room URL')}
         gaAction='copy_room_url'
-        className='mx-auto flex flex-col items-center gap-1 md:flex-row md:gap-2'
+        className='flex cursor-pointer items-center gap-1 md:gap-2'
         {...props}
       >
-        <span>Room ID</span>
-        <span className='hidden md:inline'>:</span>
+        <span className='icon-[ic--round-home] text-2xl' />
         <span>{room.id}</span>
         <span className='icon-[fa6-solid--link]' />
       </ClipboardCopyLink>

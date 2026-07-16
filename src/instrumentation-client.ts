5771ab797af2e736ee51575ad664244d9effdd00
at946 <52263024+at946@users.noreply.github.com>
2026-07-16 23:27:01 +0900

    set replaysSessionSampleRate to zero in instrumentation-client.ts

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -20,7 +20,7 @@ Sentry.init({
   // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
